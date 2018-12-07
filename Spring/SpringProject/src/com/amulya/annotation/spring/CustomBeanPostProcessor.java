2808860b976b729377b58ffde47757d549b74253
Amulya Pramidi <KH2018@kitspl.com>
2018-12-07 19:17:55 +0530

    Usage of BeanPostProcessor with annotations Signed-off-by: Amulya Pramidi <KH2018@kitspl.com>

---
+++ Spring/SpringProject/src/com/amulya/annotation/spring/CustomBeanPostProcessor.java
@@ -0,0 +1,28 @@
+package com.amulya.annotation.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor,Ordered {
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("[Before1]Inside postProcessBeforeInitialization : "+beanName);
+		return bean;
+	}
+	
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("[After1]Inside postProcessAfterInitialization : "+beanName);
+		return bean;
+	}
+
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+}
